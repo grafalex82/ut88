a9a36a37303a5c15c48097f825942f2e1fc94929
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-04-14 22:08:20 +0300

    Add memory manager to the Machine

--- src/machine.py
+++ src/machine.py
@@ -1,7 +1,57 @@
 import logging
+from utils import *
 
 logger = logging.getLogger('machine')
 
-class Machine:
+class MemoryMgr:
     def __init__(self):
         self._memories = []
+
+    def add_memory(self, memory):
+        startaddr = memory.get_start_addr()
+        endaddr = memory.get_end_addr()
+        self._memories.append((startaddr, endaddr, memory))
+
+    def get_memory_for_addr(self, addr):
+        for mem in self._memories:
+            if addr >= mem[0] and addr <= mem[1]:
+                return mem[2]
+        return None
+
+
+class Machine:
+    def __init__(self):
+        self._memories = MemoryMgr()
+
+    def add_memory(self, memory):
+        self._memories.add_memory(memory)
+
+    def _get_memory(self, addr):
+        mem = self._memories.get_memory_for_addr(addr)
+        if not mem:
+            raise MemoryError(f"No memory registered for address 0x{addr:04x}")
+        return mem
+
+    def read_memory_byte(self, addr):
+        mem = self._get_memory(addr)
+        return mem.read_byte(addr)
+
+    def read_memory_word(self, addr):
+        mem = self._get_memory(addr)
+        return mem.read_word(addr)
+
+    def write_memory_byte(self, addr, value):
+        mem = self._get_memory(addr)
+        mem.write_byte(addr, value)
+
+    def write_memory_word(self, addr, value):
+        mem = self._get_memory(addr)
+        mem.write_word(addr, value)
+
+    def push_stack(self, addr, value):
+        mem = self._get_memory(addr)
+        mem.push(addr, value)
+
+    def pop_stack(self, addr):
+        mem = self._get_memory(addr)
+        return mem.pop(addr)
--- test/test_machine.py
+++ test/test_machine.py
@@ -8,10 +8,34 @@ sys.path.append('../src')
 
 from machine import Machine
 from utils import *
+from ram import RAM
+from rom import ROM
 
 @pytest.fixture
 def machine():
-    return Machine() 
+    m = Machine()
+    m.add_memory(ROM("../resources/Monitor0.bin", 0x4000))
+    m.add_memory(RAM(0x8000, 0x8fff))
+    return m
 
-def test_machine_create(machine):
-    pass
+def test_ram_read_write(machine):
+    assert machine.read_memory_byte(0x8765) == 0x00
+    machine.write_memory_byte(0x8765, 0x42)
+    assert machine.read_memory_byte(0x8765) == 0x42
+
+    assert machine.read_memory_word(0x8642) == 0x0000
+    machine.write_memory_word(0x8642, 0xbeef)
+    assert machine.read_memory_word(0x8642) == 0xbeef
+
+    assert machine.pop_stack(0x8ace) == 0x0000
+    machine.push_stack(0x8ace, 0xbeef)
+    assert machine.pop_stack(0x8acc) == 0xbeef
+
+def test_rom_read(machine):
+    assert machine.read_memory_byte(0x4042) == 0x26
+    assert machine.read_memory_word(0x4242) == 0x09e5
+
+def test_memory_addr_validation(machine):
+    with pytest.raises(MemoryError) as e:
+        machine.read_memory_byte(0x1234)
+    assert "No memory registered for address 0x1234" in str(e.value)
