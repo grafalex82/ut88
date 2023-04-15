47563787324f30d944fe6f13efc23909dd4932d7
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-04-15 18:38:20 +0300

    Improve CPU tests naming

--- test/test_cpu.py
+++ test/test_cpu.py
@@ -18,7 +18,7 @@ def cpu():
     machine.add_memory(RAM(0x0000, 0xffff))
     return CPU(machine) 
 
-def test_cpu_reset_values(cpu):
+def test_reset_values(cpu):
     assert cpu._a == 0x00
     assert cpu._b == 0x00
     assert cpu._c == 0x00
@@ -37,13 +37,13 @@ def test_cpu_reset_values(cpu):
 
     assert cpu._cycles == 0
 
-def test_cpu_nop(cpu):
+def test_nop(cpu):
     cpu._machine.write_memory_byte(0x0000, 0x00)    # Instruction Opcode
     cpu.step()
     assert cpu._pc == 0x0001
     assert cpu._cycles == 4
 
-def test_cpu_lxi_bc(cpu):
+def test_lxi_bc(cpu):
     cpu._machine.write_memory_byte(0x0000, 0x01)    # Instruction Opcode
     cpu._machine.write_memory_word(0x0001, 0xbeef)  # Address
     cpu.step() 
@@ -52,7 +52,7 @@ def test_cpu_lxi_bc(cpu):
     assert cpu._c == 0xef
     assert cpu._cycles == 10
 
-def test_cpu_lxi_de(cpu):
+def test_lxi_de(cpu):
     cpu._machine.write_memory_byte(0x0000, 0x11)    # Instruction Opcode
     cpu._machine.write_memory_word(0x0001, 0xbeef)  # Address
     cpu.step() 
@@ -61,7 +61,7 @@ def test_cpu_lxi_de(cpu):
     assert cpu._e == 0xef
     assert cpu._cycles == 10
     
-def test_cpu_lxi_hl(cpu):
+def test_lxi_hl(cpu):
     cpu._machine.write_memory_byte(0x0000, 0x21)    # Instruction Opcode
     cpu._machine.write_memory_word(0x0001, 0xbeef)  # Address
     cpu.step() 
@@ -70,7 +70,7 @@ def test_cpu_lxi_hl(cpu):
     assert cpu._l == 0xef
     assert cpu._cycles == 10
     
-def test_cpu_lxi_bc(cpu):
+def test_lxi_bc(cpu):
     cpu._machine.write_memory_byte(0x0000, 0x31)    # Instruction Opcode
     cpu._machine.write_memory_word(0x0001, 0xbeef)  # Address
     cpu.step() 
@@ -78,56 +78,56 @@ def test_cpu_lxi_bc(cpu):
     assert cpu._sp == 0xbeef
     assert cpu._cycles == 10
 
-def test_cpu_mvi_a(cpu):
+def test_mvi_a(cpu):
     cpu._machine.write_memory_byte(0x0000, 0x3e)    # Instruction Opcode
     cpu._machine.write_memory_byte(0x0001, 0x42)    # Value
     cpu.step()
     assert cpu._a == 0x42
     assert cpu._cycles == 7
 
-def test_cpu_mvi_b(cpu):
+def test_mvi_b(cpu):
     cpu._machine.write_memory_byte(0x0000, 0x06)    # Instruction Opcode
     cpu._machine.write_memory_byte(0x0001, 0x42)    # Value
     cpu.step()
     assert cpu._b == 0x42
     assert cpu._cycles == 7
 
-def test_cpu_mvi_c(cpu):
+def test_mvi_c(cpu):
     cpu._machine.write_memory_byte(0x0000, 0x0e)    # Instruction Opcode
     cpu._machine.write_memory_byte(0x0001, 0x42)    # Value
     cpu.step()
     assert cpu._c == 0x42
     assert cpu._cycles == 7
 
-def test_cpu_mvi_d(cpu):
+def test_mvi_d(cpu):
     cpu._machine.write_memory_byte(0x0000, 0x16)    # Instruction Opcode
     cpu._machine.write_memory_byte(0x0001, 0x42)    # Value
     cpu.step()
     assert cpu._d == 0x42
     assert cpu._cycles == 7
 
-def test_cpu_mvi_e(cpu):
+def test_mvi_e(cpu):
     cpu._machine.write_memory_byte(0x0000, 0x1e)    # Instruction Opcode
     cpu._machine.write_memory_byte(0x0001, 0x42)    # Value
     cpu.step()
     assert cpu._e == 0x42
     assert cpu._cycles == 7
 
-def test_cpu_mvi_h(cpu):
+def test_mvi_h(cpu):
     cpu._machine.write_memory_byte(0x0000, 0x26)    # Instruction Opcode
     cpu._machine.write_memory_byte(0x0001, 0x42)    # Value
     cpu.step()
     assert cpu._h == 0x42
     assert cpu._cycles == 7
 
-def test_cpu_mvi_l(cpu):
+def test_mvi_l(cpu):
     cpu._machine.write_memory_byte(0x0000, 0x2e)    # Instruction Opcode
     cpu._machine.write_memory_byte(0x0001, 0x42)    # Value
     cpu.step()
     assert cpu._l == 0x42
     assert cpu._cycles == 7
 
-def test_cpu_mvi_m(cpu):
+def test_mvi_m(cpu):
     cpu._machine.write_memory_byte(0x0000, 0x36)    # Instruction Opcode
     cpu._machine.write_memory_byte(0x0001, 0x42)    # Value
     cpu._set_hl(0x1234)
@@ -135,14 +135,14 @@ def test_cpu_mvi_m(cpu):
     assert cpu._machine.read_memory_byte(0x1234) == 0x42
     assert cpu._cycles == 10
 
-def test_cpu_jmp(cpu):
+def test_jmp(cpu):
     cpu._machine.write_memory_byte(0x0000, 0xc3)    # Instruction Opcode
     cpu._machine.write_memory_word(0x0001, 0xbeef)  # Address
     cpu.step()
     assert cpu._pc == 0xbeef
     assert cpu._cycles == 10
 
-def test_cpu_ei_di(cpu):
+def test_ei_di(cpu):
     cpu._machine.write_memory_byte(0x0000, 0xfb)    # Instruction Opcode
     cpu._machine.write_memory_byte(0x0001, 0xf3)    # Instruction Opcode
     
