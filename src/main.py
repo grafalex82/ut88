38dc3b87cc3ed71575f064e00f481effd6b6fcc2
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-04-16 18:52:07 +0300

    Suppress logging for RST3 and RST4 subroutines of the Monitor0 firmware

--- src/main.py
+++ src/main.py
@@ -7,6 +7,12 @@ from rom import ROM
 from lcd import LCD
 from hexkbd import HexKeyboard
 
+def enable_debug_logging():
+    logging.disable(logging.NOTSET)
+
+def disable_debug_logging():
+    logging.disable(logging.DEBUG)
+
 def main():
     logging.basicConfig(level=logging.DEBUG)
 
@@ -20,6 +26,12 @@ def main():
     machine.add_io(kbd)
 
     emulator = Emulator(machine)
+    emulator.add_breakpoint(0x0018, disable_debug_logging)  # Suppress logging for RST3 (wait 1s)
+    emulator.add_breakpoint(0x0018, lambda: print("RST 3: Wait 1s"))
+    emulator.add_breakpoint(0x005e, enable_debug_logging)
+    emulator.add_breakpoint(0x0021, disable_debug_logging)  # Suppress logging for RST4 (wait for a button)
+    emulator.add_breakpoint(0x0021, lambda: print("RST 4: Wait a button"))
+    emulator.add_breakpoint(0x006d, enable_debug_logging)
     emulator.run()
 
 if __name__ == '__main__':
