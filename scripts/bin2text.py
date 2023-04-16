a33670ba36e1442a516527d50d5c3c2ca64c2871
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-04-16 11:53:04 +0300

    Add binary file to hex text form converter

---
+++ scripts/bin2text.py
@@ -0,0 +1,23 @@
+import argparse
+import sys
+
+def main():
+    parser = argparse.ArgumentParser(
+                    prog='bin2text',
+                    description='Binary to hexadecimal textual view converter')
+    parser.add_argument('binfile')
+    args = parser.parse_args()
+
+    with open(args.binfile, mode="rb") as f:
+        bin = f.read()
+
+        addr = 0
+        for c in bin:
+            if addr % 0x10 == 0:
+                sys.stdout.write(f"\n{addr:04x} ")
+            sys.stdout.write(f" {int(c):02x}")
+            addr += 1
+            
+
+if __name__ == '__main__':
+    main()
