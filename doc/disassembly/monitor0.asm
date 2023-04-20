f3492ac1b89b597595269fcf951912ff64d5abf2
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-04-20 17:22:48 +0300

    Finish Monitor0 basic part disassembly.

--- doc/disassembly/monitor0.asm
+++ doc/disassembly/monitor0.asm
@@ -1,6 +1,14 @@
-; Monitor0 is a primary firmware for the CPU module. Monitor0 is located in ROM
-; at 0x0000-0x03ff address and provides basic routines for CPU module peripheral:
-; - TBD
+; Monitor0 is a primary firmware for the CPU module. Monitor0 is located in ROM at
+; 0x0000-0x03ff address and provides basic routines for CPU module peripheral. The Monitor0
+; is split into 2 parts
+; - 0x0000-0x01ff - essential part of the ROM that includes
+;     - Handy routines to input data from the keyboard, output to LCD, input and output
+;       the data to the tape.
+;     - Basic operating system to read/write memory and ROM, execute programs
+;     - Current time clock
+; - 0x0200-0x03ff - optional part with a few useful programs:
+;     - memory copying programs
+;     - TBD 
 ;
 ; Monitor0 exposes a few handy routines for the purposes of Monitor itself and the
 ; user application. Unlike typical approach, when CALL 3-byte instruction is used to
@@ -10,11 +18,13 @@
 ;
 ; RST routines are:
 ; - RST 0 (address 0x0000)  - reset routine
+; - RST 1 (address 0x0008)  - output a byte in A to the tape
 ; - RST 2 (address 0x0010)  - wait for a byte (2 digit) pressed on the keypad, return in A
 ; - RST 3 (address 0x0018)  - 1 second delay
 ; - RST 4 (address 0x0020)  - wait for a button press, return key code in A
 ; - RST 5 (address 0x0028)  - display A and HL registers on the LCD
 ; - RST 6 (address 0x0030)  - wait for a 2 byte (4 digit) value typed on the keyboard, return in DE
+; - RST 7 (address 0x0038)  - time interrupt (executed every second, and advances time value)
 ; 
 ; When Monior0 starts, it displays 11 on the rightmost LCD digits - this is a ready signal.
 ; The Monitor0 interacts with the user with the following commands (entered using HEX keyboard)
@@ -37,15 +47,34 @@
 ; - 6           - Start the program starting address 0xc000
 ; - 7 <addr>    - Start the program starting the user address
 ; - 8 <a1> <a2> - Calculate CRC for the address range a1-a2
+; - 9 <a1> <a2> - Store data at the address range a1-a2 to the tape
+; - A <offset>  - Read data from tape with the offset (to the start address written on the tape)
 ; - B           - Display current time (0x3cfd - seconds, 0x3cfe - minutes, 0x3cff - hours)
+; - C <addr>    - Enter new time. Same as command 0, but interrupts disabled. Address shall be 0x3cfd
+;
+; Important memory adresses:
+; - 0x3fc       - tape reading polarity (0x00 - non inverted, 0xff - inverted)
+; - 0x3fd       - seconds
+; - 0x3fe       - minutes
+; - 0x3ff       - hours
+;
+; Tape recording format:
+; - 256 x 0xff  - pilot tone, to sync with the sequence
+; - 0xe6        - synchronization byte, marks polarity and start of data bytes
+; - 2 bytes     - start address
+; - 2 bytes     - end address
+; - <bytes>     - data bytes
+;
+; No CRC bytes are stored on the tape. The CRC value is displayed on the screen after store
+; and load commands. The User is responsible for validating the CRC.
 ;
 ; Bugs and issues:
 ; - RST4 is a handy routine for waiting a keyboard input. But Step Back button is also 
 ;   processed in the RST4. And instead of returning the step back button code to the caller
 ;   it rudely switches to RAM write (Command 0) execution, regardless of what was executed before
-; - Command 8 gently saves all the registers, and restores them back. But it ends up calling a
-;   RST0 (reset) function eventually. So the code could save 8 bytes as no one really interested
-;   in those registers
+; - Commands 8, 9 and C gently save all the registers, and restores them back. But it ends up calling
+;   a RST0 (reset) function eventually. So the code could save at least 16 bytes as no one really
+;   interested in those registers
 ; - Command B (display current time) uses RST 3 delay routine between displaying a new time value.
 ;   This causes a non-uniform displaying, some values are skipped
 
@@ -59,9 +88,9 @@ RST0:
     0003  3e 11      MVI A, 11
     0005  c3 3b 00   JMP RST0_CONT (003b)   ; Continuate elsewhere, as RST1 handler is located at 0x0008
 
+; RST 1 - Output data byte to the tape
 RST1:
-0008    ...
-0000                          c3 00 01
+    0008  c3 00 01   JMP RST_1_CONT (0100)
 
 ; Command 5 - read memory data at the provided address
 CMD_5:
@@ -313,13 +342,13 @@ TIMER_ADV:
     00e4             60, 60, 24             ; Maximum values for seconds, minutes, and hours
 
 
-00e0                       c3 9a 01 c3 c2 01 
-
+; Command 9 - store a data range to the tape
 CMD_9:
-    00e7
+    00e7  c3 9a 01   JMP CMD_9_CONT (019a)  ; Continue elsewhere (out of the first 0x100 bytes)
 
+; Command A - read data from tape
 CMD_A:
-    00ea
+    00ea  c3 c2 01   JMP CMD_A_CONT (01c2)
 
 ; Command 8 - CRC of the memory region
 CMD_8:
@@ -333,15 +362,117 @@ CMD_B:
 CMD_HANDLERS:
     db    91, 8a, 7a, 96, a3, 0b, 25, 84, ed, e7, ea, f0, 90       
 
+; RST 1 - Output data byte to the tape
+RST_1_CONT:
+    0100  c5         PUSH BC                ; Save affected registers
+    0101  d5         PUSH DE
+    0102  f5         PUSH PSW
 
-0100  c5 d5 f5 57 0e 08 7a 07 57 3e 01 aa d3 a1 cd 21
-0110  01 3e 00 aa d3 a1 cd 21 01 0d c2 06 01 f1 d1 c1
-0120  c9 06 1e 05 c2 23 01 c9 c5 d5 0e 00 57 db a1 5f
-0130  79 e6 7f 07 4f db a1 bb ca 35 01 e6 01 b1 4f cd
-0140  6e 01 db a1 5f 7a b7 f2 63 01 79 fe e6 c2 57 01
-0150  af 32 fc c3 c3 61 01 fe 19 c2 30 01 3e ff 32 fc
-0160  c3 16 09 15 c2 30 01 3a fc c3 a9 d1 c1 c9 06 2d
-0170  05 c2 70 01 c9 
+    0103  57         MOV D, A               ; Save the byte to send
+    0104  0e 08      MVI C, 08              ; 8 bits to send
+
+OUT_NEXT_BIT:
+    0106  7a         MOV A, D               ; Get next bit into carry flag
+    0107  07         RLC
+    0108  57         MOV D, A
+
+    0109  3e 01      MVI A, 01              ; Output least significant bit inverted
+    010b  aa         XRA D
+    010c  d3 a1      OUT a1
+    010e  cd 21 01   CALL TAPE_DELAY (0121)
+
+    0111  3e 00      MVI A, 00              ; Output least significant bit non inverted
+    0113  aa         XRA D
+    0114  d3 a1      OUT a1
+    0116  cd 21 01   CALL TAPE_DELAY (0121)
+
+    0119  0d         DCR C                  ; Repeat until all bits are sent
+    011a  c2 06 01   JNZ OUT_NEXT_BIT (0106)
+
+    011d  f1         POP PSW                ; Restore registers and exit
+    011e  d1         POP DE
+    011f  c1         POP BC
+    0120  c9         RET
+
+; A small delay function, used to measure bit length while storing to tape
+TAPE_DELAY:
+    0121  06 1e      MVI B, 1e              ; Delay counter
+TAPE_DELAY_LOOP:
+    0123  05         DCR B
+    0124  c2 23 01   JNZ TAPE_DELAY_LOOP (0123)
+    0127  c9         RET
+
+
+; Receive a byte from the tape. A - number of bits to receive, or 0xff to wait for synchronization
+IN_TAPE_BYTE:
+    0128  c5         PUSH BC
+    0129  d5         PUSH DE
+    012a  0e 00      MVI C, 00              ; register C - Data byte accumulator
+    012c  57         MOV D, A               ; register D - Number of bits to listen (or 0xff for sync)
+    
+    012d  db a1      IN a1                  ; Input a bit from the tape
+    012f  5f         MOV E, A
+    
+IN_NEXT_BIT:
+    0130  79         MOV A, C               ; Prepare current byte accumulator for receiving next bit
+    0131  e6 7f      ANI 7f
+    0133  07         RLC
+    0134  4f         MOV C, A
+
+WAIT_NEXT_PHASE:
+    0135  db a1      IN a1                  ; Wait for the bit phase change
+    0137  bb         CMP E
+    0138  ca 35 01   JZ WAIT_NEXT_PHASE (0135)
+
+    013b  e6 01      ANI 01                 ; Received bit is the data bit
+    013d  b1         ORA C                  ; Store the value in C
+    013e  4f         MOV C, A
+
+    013f  cd 6e 01   CALL TAPE2_DELAY (016e); Wait for the next bit
+
+    0142  db a1      IN a1                  ; Input next bit
+    0144  5f         MOV E, A
+
+    0145  7a         MOV A, D               ; If synchronization is already happened - repeat
+    0146  b7         ORA A                  ; needed number of bytes, otherwise (D == 0xff) wait
+    0147  f2 63 01   JP BIT_RECEIVED (0163) ; for a sync byte
+
+    014a  79         MOV A, C               ; Check if synchronization byte has been received
+    014b  fe e6      CPI e6
+    014d  c2 57 01   JNZ TRY_SYNC_NEGATIVE (0157)
+
+    0150  af         XRA A                  ; Synchronization happen in positive polarity
+    0151  32 fc c3   STA c3fc               ; Remember this fact in 0xc3fc, no bytes inversion will be performed
+    0154  c3 61 01   JMP IN_SYNCHRONIZED (0161)
+
+TRY_SYNC_NEGATIVE:
+    0157  fe 19      CPI 19                 ; Try another polarity of the sync byte (e6 inverted)
+    0159  c2 30 01   JNZ IN_NEXT_BIT (0130) ; If sync has not happen - wait for the next bit
+
+    015c  3e ff      MVI A, ff              ; Synchronization happened in reverse polarity
+    015e  32 fc c3   STA c3fc               ; Remember this in 0xc3fc, and invert all received bytes
+
+IN_SYNCHRONIZED:
+    0161  16 09      MVI D, 09              ; Prepare for receiving a real data byte
+ 
+BIT_RECEIVED:
+    0163  15         DCR D                  ; Repeat until all bits are received
+    0164  c2 30 01   JNZ 0130
+
+    0167  3a fc c3   LDA c3fc               ; Apply inverting if necessary
+    016a  a9         XRA C
+
+    016b  d1         POP DE                 ; Return received byte in A
+    016c  c1         POP BC
+    016d  c9         RET
+
+; A small delay function, used to measure bit length while loading from tape
+TAPE2_DELAY:
+    016e  06 2d      MVI B, 2d                  ; Delay counter
+TAPE2_DELAY_LOOP:
+    0170  05         DCR B
+    0171  c2 70 01   JNZ TAPE2_DELAY_LOOP (0170)
+    0174  c9         RET
 
 ; Command 8 - Calculate CRC of the memory region
 CMD_8_CONT:
@@ -356,6 +487,7 @@ CMD_8_CONT:
 
     017c  f7         RST 6                  ; Read the ending address to DE
 
+CALC_CRC:
     017d  2e 00      MVI L, 00              ; Zero CRC value accumulator in HL
     017f  65         MOV H, L
 
@@ -387,13 +519,98 @@ CMP_BC_DE:
     0198  b9         CMP C
     0199  c9         RET
 
-0190                                c5 d5 e5 f5 f7 42
-01a0  4b f7 c5 af 6f cf 2c c2 a5 01 3e e6 cf 78 cf 79
-01b0  cf 7a cf 7b cf 0a cf cd 94 01 03 c2 b5 01 c1 c3
-01c0  7d 01 c5 d5 e5 f5 f7 3e ff cd 28 01 67 cd ee 01
-01d0  6f 19 44 4d c5 cd ee 01 67 cd ee 01 6f 19 eb cd
-01e0  ee 01 02 cd 94 01 03 c2 df 01 c1 c3 7d 01 3e 08
-01f0  cd 28 01 c9 00 
+
+; Command 9 - store a data range to the tape
+CMD_9_CONT:
+    019a  c5         PUSH BC
+    019b  d5         PUSH DE
+    019c  e5         PUSH HL
+    019d  f5         PUSH PSW
+
+    019e  f7         RST 6                  ; Read starting address to BC
+    019f  42         MOV B, D
+    01a0  4b         MOV C, E
+
+    01a1  f7         RST 6                  ; Read ending address to DE
+
+    01a2  c5         PUSH BC                ; Save start address to be used during CRC calculation
+    01a3  af         XRA A                  
+    01a4  6f         MOV L, A               ; L will contain byte counter
+
+OUT_SYNC_LOOP:
+    01a5  cf         RST 1                  ; Output zero byte 256 times (synchronization sequence)
+    01a6  2c         INR L                  ; 
+    01a7  c2 a5 01   JNZ OUT_SYNC_LOOP (01a5)
+
+    01aa  3e e6      MVI A, e6              ; Send synchronization byte
+    01ac  cf         RST 1
+
+    01ad  78         MOV A, B               ; Output BC register (start address)
+    01ae  cf         RST 1
+    01af  79         MOV A, C
+    01b0  cf         RST 1
+
+    01b1  7a         MOV A, D               ; Output DE register (end address)
+    01b2  cf         RST 1
+    01b3  7b         MOV A, E
+    01b4  cf         RST 1
+
+OUT_NEXT_BYTE:
+    01b5  0a         LDAX BC                ; Output next data byte
+    01b6  cf         RST 1
+
+    01b7  cd 94 01   CALL CMP_BC_DE (0194)  ; Advance to the next byte, until end address reached
+    01ba  03         INX BC
+    01bb  c2 b5 01   JNZ OUT_NEXT_BYTE (01b5)
+
+    01be  c1         POP BC                 ; Restore start address and switch to CRC calculation
+    01bf  c3 7d 01   JMP CALC_CRC (017d)
+
+; Command A - read data from tape
+CMD_A_CONT:
+    01c2  c5         PUSH BC
+    01c3  d5         PUSH DE
+    01c4  e5         PUSH HL
+    01c5  f5         PUSH PSW
+
+    01c6  f7         RST 6                  ; Read address offset to DE
+
+    01c7  3e ff      MVI A, ff              ; Wait for a synchronization, then input a data byte
+
+    01c9  cd 28 01   CALL IN_TAPE_BYTE (0128)   ; Read start address in HL
+    01cc  67         MOV H, A
+    01cd  cd ee 01   CALL IN_NEXT_BYTE (01ee)
+    01d0  6f         MOV L, A
+
+    01d1  19         DAD DE                 ; Apply offset
+    01d2  44         MOV B, H               ; Store the calculated start address at BC
+    01d3  4d         MOV C, L
+    01d4  c5         PUSH BC                
+
+    01d5  cd ee 01   CALL 01ee              ; Read end address to HL
+    01d8  67         MOV H, A
+    01d9  cd ee 01   CALL 01ee    
+    01dc  6f         MOV L, A
+
+    01dd  19         DAD DE                 ; And apply offset as well
+    01de  eb         XCHG                   ; Store the calculated end address at DE
+
+IN_LOOP:
+    01df  cd ee 01   CALL 01ee              ; Read and store the data byte
+    01e2  02         STAX B
+    01e3  cd 94 01   CALL CMP_BC_DE (0194)  ; Continue until all the data bytes received
+    01e6  03         INX B
+    01e7  cd df 01   JNZ IN_LOOP (01df)
+
+    01ea  c1         POP BC
+    01eb  c3 7d 01   JMP CALC_CRC (017d)    ; Double check the CRC and finish
+
+
+IN_NEXT_BYTE:
+    01ee  3e 08      MVI A, 08              ; Set number of bits to receive
+    01f0  cd 28 01   CALL IN_TAPE_BYTE (0128)
+    01f3  c9         RET
+    01f4  00         NOP
 
 CMD_B_CONT:
     01f5  2a fe c3   LHLD c3fe              ; Load minutes and hours to HL
