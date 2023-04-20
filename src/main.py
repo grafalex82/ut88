89545e407b898292e19065300a38a0ba33bccc8b
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-04-20 17:16:21 +0300

    Add tape emulation to the basic CPU module configuration

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from rom import ROM
 from lcd import LCD
 from hexkbd import HexKeyboard
 from timer import Timer
+from tape import TapeRecorder
 
 def enable_debug_logging():
     logging.disable(logging.NOTSET)
@@ -33,6 +34,8 @@ def main():
     machine.add_io(kbd)
     timer = Timer(machine)
     machine.add_other_device(timer)
+    recorder = TapeRecorder()
+    machine.add_io(recorder)
 
     emulator = Emulator(machine)
     emulator.add_breakpoint(0x0018, disable_debug_logging)  # Suppress logging for RST3 (wait 1s)
@@ -43,8 +46,14 @@ def main():
     emulator.add_breakpoint(0x0021, disable_debug_logging)  # Suppress logging for RST4 (wait for a button)
     emulator.add_breakpoint(0x0021, lambda: print("RST 4: Wait a button"))
     emulator.add_breakpoint(0x006d, enable_debug_logging)
-    emulator.add_breakpoint(0x006e, enable_debug_logging)
-    emulator.add_breakpoint(0x0038, enable_debug_logging)
+    emulator.add_breakpoint(0x0008, disable_debug_logging)  # Suppress logging for RST1 (output a byte)
+    emulator.add_breakpoint(0x0008, lambda: print("RST 1: Output a byte"))
+    emulator.add_breakpoint(0x0120, enable_debug_logging)
+    emulator.add_breakpoint(0x01bf, lambda: recorder.dump("test.bin"))
+    emulator.add_breakpoint(0x0128, lambda: print("Read a byte from tape"))
+    emulator.add_breakpoint(0x016e, disable_debug_logging)  # Suppress bit reading delay function
+    emulator.add_breakpoint(0x0174, enable_debug_logging)
+    emulator.add_breakpoint(0x016d, lambda: print(f"Received byte - {emulator._cpu._a:02x}"))
     
     while True:
         screen.fill(pygame.Color('black'))
@@ -53,7 +62,7 @@ def main():
             if event.type == pygame.QUIT:
                 exit()
         
-        emulator.run(10000)
+        emulator.run(50000)
 
         if pygame.key.get_pressed()[pygame.K_ESCAPE]:
             emulator.reset()
