d2a450a2924204488210fcf92cbd7b4e94c3eb2b
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-04-21 22:14:31 +0300

    Add more Monitor0 disassembly (Address correction program)

--- doc/disassembly/monitor0_2.asm
+++ doc/disassembly/monitor0_2.asm
@@ -1,4 +1,10 @@
-; Memory copying function
+; This part of the Monitor0 firmware contains a few handy programs:
+; - 0x0200  - Memory copying program
+; - 0x025f  - Address correction program (correct addresses after copying to other memory region)
+; - 0x03b2  - Memory compare program
+
+
+; Memory copying program
 ; Enters 3 parameters:
 ; - Source start address
 ; - Source end address
@@ -107,21 +113,148 @@ ENTER_ADDR:
     0255  2a f6 c3   LHLD c3f6              ; Otherwise carry bit is off, HL=dest end addr
     0258  c9         RET
 
+; Compare DE and HL register pairs, set Z if equeal
+CMD_DE_HL:
+    0259  7c         MOV A, H
+    025a  ba         CMP D
+    025b  c0         RNZ
+    025c  7d         MOV A, L
+    025d  bb         CMP E
+    025e  c9         RET
 
 
+; Memory correction program. Used to correct addresses after copying a code to another
+; memory region. The program detects 3-byte instructions, and corrects their arguments
+; if they are in the source address region.
+; 
+; Enters 3 parameters:
+; - Source start address
+; - Source end address
+; - Destination start address
+MEM_CORR:
+    025f  cd 24 02   CALL ENTER_ADDR (0224) ; Enter addresses to work
+    0262  cd 66 02   CALL DO_MEM_CORR (0266); Execute the actual program
+    0265  c7         RST 0                  ; The end (reset)
+
+DO_MEM_CORR:
+    0266  2a f4 c3   LHLD c3f4              ; Load destination start address in HL
+
+MEM_CORR_LOOP:
+    0269  56         MOV D, M               ; Load next byte and match it with an opcode
+    026a  e5         PUSH HL
+    026b  cd b9 02   CALL MATCH_OPCODE (02b9)
+
+    026e  60         MOV H, B               ; Restore current address in HL, but store B (instruction
+    026f  e3         XTHL                   ; size) on the stack instead
+
+    0270  78         MOV A, B               ; Check if 3-byte instruction was matched
+    0271  fe 03      CPI 03
+    0273  c2 a5 02   JNZ MEM_CORR_2 (02a5)
+
+    0276  23         INX HL                 ; 3-byte instruction matched - patch the 2-byte argument
+    0277  4e         MOV C, M               ; Load the argument to BC
+    0278  23         INX HL
+    0279  46         MOV B, M
+
+    027a  2b         DCX HL
+    027b  e5         PUSH HL
+
+    027c  2a f0 c3   LHLD c3f0              ; Compare instruction argument with 
+    027f  79         MOV A, C               ; the source start. Skip patching if
+    0280  95         SUB L                  ; the argument is below source start address
+    0281  78         MOV A, B
+    0282  9c         SBB H
+    0283  da a3 02   JC MEM_CORR_1 (02a3)
+
+    0286  2a f2 c3   LHLD c3f2              ; Compare instruction argument with the
+    0289  7d         MOV A, L               ; Source end address. Skip patching if the
+    028a  91         SUB C                  ; argument is above source end address
+    028b  7c         MOV A, H
+    028c  98         SBB B
+    028d  da a3 02   JC MEM_CORR_1 (02a3)
+
+    0290  2a f8 c3   LHLD c3f8              ; Load the source-destination address difference to HL
+    0293  7d         MOV A, L               ; Add the difference to instruction argument
+    0294  81         ADD C
+    0295  5f         MOV E, A               ; Store result to DE
+    0296  7c         MOV A, H
+    0297  88         ADC B
+    0298  57         MOV D, A
+
+    0299  e1         POP HL                 ; Patch the instruction argument
+    029a  73         MOV M, E
+    029b  23         INX H
+    029c  72         MOV M, D
+    029d  23         INX H
+    02be  33         INX SP
+    02bf  33         INX SP
+
+    02a0  c3 ab 02   JMP MEM_CORR_NEXT (02ab)   ; Advance to the next instruction
+
+MEM_CORR_1:
+    02a3  e1         POP HL                 ; Just restore current instruction address (HL)
+    02a4  2b         DCX HL
+
+MEM_CORR_2:
+    02a5  c1         POP BC                 ; And instruction size
+
+MEM_CORR_L:
+    02a6  23         INX HL                 ; Advance HL by instruction size
+    02a7  05         DCR B
+    02a8  c2 a6 02   JNZ MEM_CORR_L (02a6)
+
+MEM_CORR_NEXT:
+    02ab  5d         MOV E, L               ; Repeat until destination address is reached
+    02ac  54         MOV D, H
+    02ad  2a f6 c3   LHLD c3f6
+    02b0  23         INX HL
+    02b1  cd 59 02   CALL CMD_DE_HL (0259)
+    02b4  eb         XCHG
+    02b5  c2 69 02   JNZ MEM_CORR_LOOP (0269)
+    02b8  c9         RET
 
 
+; Match an opcode given in D with a instructions matching table
+; Return Z=1 if instruction matches (B=instruction length)
+; Z=0 if no match
+MATCH_OPCODE:
+    02b9  01 06 03   LXI BC, 0306           ; Start with 3-byte instructions, 6 types
+    02bc  21 d3 02   LXI HL, 02d3           ; Load opcode mask table address
 
-0250                             7c ba c0 7d bb c9 cd
-0260  24 02 cd 66 02 c7 2a f4 c3 56 e5 cd b9 02 60 e3
-0270  78 fe 03 c2 a5 02 23 4e 23 46 2b e5 2a f0 c3 79
-0280  95 78 9c da a3 02 2a f2 c3 7d 91 7c 98 da a3 02
-0290  2a f8 c3 7d 81 5f 7c 88 57 e1 73 23 72 23 33 33
-02a0  c3 ab 02 e1 2b c1 23 05 c2 a6 02 5d 54 2a f6 c3
-02b0  23 cd 59 02 eb c2 69 02 c9 01 06 03 21 d3 02 7a
-02c0  a6 23 be c8 23 0d c2 bf 02 0e 03 05 78 fe 01 c2
-02d0  bf 02 c9 ff cd c7 c4 ff c3 c7 c2 e7 22 cf 01 c7
-02e0  06 c7 c6 f7 d3 f7 eb 22 f0 c3 22 f4 c3 e5 f7 eb
+MATCH_LOOP:
+    02bf  7a         MOV A, D               ; Check whether the given byte matches instruction
+    02c0  a6         ANA M                  ; in the table
+    02c1  23         INX HL5
+    02c2  be         CMP M
+    02c3  c8         RZ                     ; Return with Z flag in case of match, B - instruction length
+
+    02c4  23         INX HL                 ; Repeat C times (6 for 3-byte instructions, 3 for 2-byte)
+    02c5  0d         DCR C
+    02c6  c2 bf 02   JNZ MATCH_LOOP (02bf)
+
+    02c9  0e 03      MVI C, 03
+    02cb  05         DCR B
+    02cc  78         MOV A, B
+    02cd  fe 01      CPI 01
+    02cf  c2 bf 02   JNZ 02bf    
+    02d2  c9         RET                    ; Zero Z flag means no match
+
+    ; 3-byte instructions masks
+    02d3 ff cd                              ; Match for CALL opcode
+    02d5 c7 c4                              ; Match for conditional CALL opcodes
+    02d7 ff c3                              ; Match for JMP opcode
+    02d9 c7 c2                              ; Match for conditional JMP opcodes
+    02db e7 22                              ; Match for SHLD, LHLD, STA, LDA opcodes
+    02dd cf 01                              ; Match for LXI opcodes
+    
+    ; 2-byte instructions masks
+    02df c7 06                              ; Match for MVI opcodes
+    02e1 c7 c6                              ; Match for ADI, ACI, SUI, SBI, ANI, XRI, ORI and CPI instructions
+    02e3 f7 d3                              ; Match for IN and OUT instructions
+
+   
+     
+02e0                 f7 eb 22 f0 c3 22 f4 c3 e5 f7 eb
 02f0  22 f2 c3 22 f6 c3 f7 eb 22 fa c3 d1 7d 93 6f 7c
 0300  9a 67 22 f8 c3 cd 66 02 c7 f7 d5 f7 eb 22 f2 c3
 0310  f7 eb 22 fa c3 f7 eb 22 ee c3 e1 22 f0 c3 56 e5
@@ -162,7 +295,7 @@ MEM_CMP:
 
 MEM_CMP_LOOP:
     03ba  0a         LDAX BC                ; Compare bytes at [BC] and [HL]
-    03bb  be         CMP M]
+    03bb  be         CMP M
     03bc  c2 d4 03   JNZ MEM_CMP_ERR (03d4) ; Display the memory mismatch error
 
     03bf  79         MOV A, C               ; Check if end address (in DE) is reached
