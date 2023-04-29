53d30b468c03c9bb27d51ebbcdfea9ade6fbed5c
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-04-29 19:43:10 +0300

    Add breakpoints possibility for single step operations as well

--- src/emulator.py
+++ src/emulator.py
@@ -13,20 +13,20 @@ class Emulator:
             self._breakpoints[addr] = []    
         self._breakpoints[addr].append(fn)
 
-    def step(self):
-        self._cpu.step()
-
     def _handle_breakpoints(self):
         # Run the breakpoint function if condition is met
         br_list = self._breakpoints.get(self._cpu._pc, [])
         for br in br_list:  
             br()
 
+    def step(self):
+        self._handle_breakpoints()
+        self._cpu.step()
+
     def run(self, num_cycles=0):
         stop_at = self._cpu._cycles + num_cycles
         while num_cycles == 0 or self._cpu._cycles <= stop_at:
-            self._handle_breakpoints()
-            self._cpu.step()
+            self.step()
 
     def reset(self):
         self._machine.reset()
