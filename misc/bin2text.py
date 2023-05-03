14b9c1230c37deb329df094521debde1e507eaba
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-05-03 18:41:13 +0300

    Bin2Text: add possibility to specify start address

--- misc/bin2text.py
+++ misc/bin2text.py
@@ -6,12 +6,16 @@ def main():
                     prog='bin2text',
                     description='Binary to hexadecimal textual view converter')
     parser.add_argument('binfile')
+    parser.add_argument('startaddr', nargs="?")
     args = parser.parse_args()
 
     with open(args.binfile, mode="rb") as f:
         bin = f.read()
 
         addr = 0
+        if args.startaddr:
+            addr = int(args.startaddr, 16)
+
         for c in bin:
             if addr % 0x10 == 0:
                 sys.stdout.write(f"\n{addr:04x} ")
