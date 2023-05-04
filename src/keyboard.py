c7087d3fe5e371f716b9d51d7dd9c9cabd3f491d
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-05-04 15:22:50 +0300

    Add keyboard ports emulation (returns 0xff for now, actual key presses are not implemented)

--- src/keyboard.py
+++ src/keyboard.py
@@ -4,18 +4,37 @@ from interfaces import *
 class Keyboard(IODevice):
     def __init__(self):
         IODevice.__init__(self, 0x04, 0x07)
-        self._configuration = 0
+        # The only supported configuration is Port A output, B and C - input
+        self.configure(0x8b)
+
+        self._port_a = 0xff
 
 
     def configure(self, value):
         self._configuration = value
+        assert self._configuration == 0x8b  
 
+
+    def read_io(self, addr):
+        # Address bits are inverted comparing to i8255 addresses
+        if addr == 0x06:            # Port B
+            return 0xff
+        if addr == 0x05:            # Port C
+            return 0xff
+
+        raise IOError(f"Reading IO {addr:x} is not supported")
 
     def write_io(self, addr, value):
         self.validate_addr(addr)
 
-        if addr == 0x04:
-            self.configure(value)
+        # Address bits are inverted comparing to i8255 addresses
+        if addr == 0x04:            # Configuration register
+            return self.configure(value)
+        if addr == 0x07:            # Port A
+            self._port_a = value         
+            return
+        
+        IOError(f"Writing IO {addr:x} is not supported")
 
     
 
