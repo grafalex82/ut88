7f96ca3d57b0c2ae1aad220a99183d78d52e3455
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-10-02 23:07:08 +0300

    Add i8257 DMA controller emulation

---
+++ src/dma.py
@@ -0,0 +1,129 @@
+from utils import *
+from interfaces import *
+
+"""
+The Intel 8257 DMA controller emulator.
+
+This class mimics the Intel 8257 DMA controller, providing the following features:
+- controller configuration ports for setting up data transfer
+- Burst data transfer (read or write) using one of 4 channels
+- autoload feature on the channel 2
+
+The controller reside on 0xe000-0xe008 memory addresses for configuration registers, that match Radio-86RK
+schematics.
+
+Note, that this class provides a very limited implementation, and does not provide all the features
+of the original controller. Just a few features that enough to run Radio-86RK use case.
+"""
+class DMA(MemoryDevice):
+    
+    def __init__(self, machine):
+        MemoryDevice.__init__(self, 0xe000, 0xe008)
+
+        self._machine = machine
+
+        self._autoload = False
+        self._tc_stop = False
+        self._extended_write = False
+        self._rotating_priority = False
+        self._channels = [{
+            'enabled': False, 
+            'start_addr': None, 
+            'count': None, 
+            'read':False, 
+            'write':False} for _ in range(4)]
+
+
+    def _enable_channel(self, channel, enable):
+        # Store the enable flag
+        self._channels[channel]['enabled'] = enable
+
+        # Nothing to do if the channel is disabled
+        if not enable:
+            return
+        
+        # Just assert we are prepared for data transfer
+        assert self._channels[channel]['start_addr'] != None
+        assert self._channels[channel]['count'] != None
+
+
+    def set_register_value(self, channel, count_register, value):
+        if count_register:
+            if self._channels[channel]['count']:
+                self._channels[channel]['count'] |= (value & 0x3f) << 8         # High byte
+                self._channels[channel]['read'] = is_bit_set(value, 6)
+                self._channels[channel]['write'] = is_bit_set(value, 7)
+            else:
+                self._channels[channel]['count'] = value & 0xff                 # Low byte
+        else:
+            if self._channels[channel]['start_addr']:
+                self._channels[channel]['start_addr'] |= (value & 0xff) << 8    # High byte
+            else:
+                self._channels[channel]['start_addr'] = value & 0xff            # Low byte
+
+        # In case of Autoload option, channel 3 registers will store original values for start address 
+        # and bytes count for channel 2
+        if self._autoload and channel == 2:
+            self._channels[3]['count'] = self._channels[2]['count']
+            self._channels[3]['start_addr'] = self._channels[2]['start_addr']
+
+
+    def get_register_value(self, channel, count_register):
+        if count_register:
+            return self._channels[channel]['count']
+        else:
+            return self._channels[channel]['start_addr']
+            
+
+    def write_byte(self, addr, value):
+        self.validate_addr(addr)
+
+        match addr:
+            case addr if addr < 0xe008:     # Start address or counter register
+                channel = (addr >> 1) & 0x03
+                count_register = is_bit_set(addr, 0)
+                self.set_register_value(channel, count_register, value)
+            case 0xe008:            # Mode byte
+                self._autoload = is_bit_set(value, 7)
+                self._tc_stop = is_bit_set(value, 6)
+                self._extended_write = is_bit_set(value, 5)
+                self._rotating_priority = is_bit_set(value, 4)
+                self._enable_channel(3, is_bit_set(value, 3))
+                self._enable_channel(2, is_bit_set(value, 2))
+                self._enable_channel(1, is_bit_set(value, 1))
+                self._enable_channel(0, is_bit_set(value, 0))
+            case _:
+                raise MemoryError(f"Writing address 0x{addr:04x} is not supported")
+
+
+    def _get_transfer_settings(self, channel):
+        if not self._channels[channel]['enabled']:
+            raise RuntimeError(f"DMA Channel {channel} is not configured for data transfer")
+
+        start_addr = self._channels[channel]['start_addr']
+        count = self._channels[channel]['count'] 
+
+        # Autoload feature will reload channel 2 settings from channel 3
+        # If no autoload feature is on, or other channel is used, the channel will be disabled
+        if channel == 2 and self._autoload:
+            self._channels[2]['start_addr'] = self._channels[3]['start_addr']
+            self._channels[2]['count'] = self._channels[3]['count']
+        else:
+            self._channels[channel]['enabled'] = False
+
+        return start_addr, count
+
+
+    def dma_read(self, channel):
+        start_addr, count = self._get_transfer_settings(channel)
+        assert self._channels[channel]['read']
+        return self._machine.read_memory_burst(start_addr, count)
+
+
+    def dma_write(self, channel, data):
+        start_addr, count = self._get_transfer_settings(channel)
+        assert self._channels[channel]['write']
+        assert len(data) == count
+
+        self._machine.write_memory_burst(start_addr, data)
+
---
+++ test/test_dma.py
@@ -0,0 +1,135 @@
+# To run these tests install pytest, then run this command line:
+# py.test -rfeEsxXwa --verbose --showlocals
+
+import pytest
+import sys
+
+sys.path.append('../src')
+
+from dma import DMA
+from ram import RAM
+from machine import Machine
+from utils import *
+
+@pytest.fixture
+def dma():
+    ram = RAM(0x0000, 0xffff)
+    machine = Machine()
+    machine.add_memory(ram)
+    return DMA(machine)
+
+def test_fill_channel_params(dma):
+    dma.write_byte(0xe002, 0x34)    # Fill start address for Channel 1 (low byte first)
+    dma.write_byte(0xe002, 0x12)
+    dma.write_byte(0xe003, 0x78)    # Fill count for Channel 1 (low byte first)
+    dma.write_byte(0xe003, 0x56)    
+
+    assert dma.get_register_value(1, False) == 0x1234   # Start address register
+    assert dma.get_register_value(1, True) == 0x1678    # Counter register (not including read/write bits)
+
+
+def test_fill_autoload_params(dma):
+    dma.write_byte(0xe008, 0x80)    # Set Autoload flag in the configuration register
+    dma.write_byte(0xe004, 0x34)    # Fill start address for Channel 1 (low byte first)
+    dma.write_byte(0xe004, 0x12)
+    dma.write_byte(0xe005, 0x78)    # Fill count for Channel 1 (low byte first)
+    dma.write_byte(0xe005, 0x56)    
+
+    assert dma.get_register_value(2, False) == 0x1234   # Start address register
+    assert dma.get_register_value(2, True) == 0x1678    # Counter register (not including read/write bits)
+    assert dma.get_register_value(3, False) == 0x1234   # Validate data is also replicated for channel 3
+    assert dma.get_register_value(3, True) == 0x1678
+
+
+def test_dma_read(dma):
+    # Fill some memory bytes
+    dma._machine.write_memory_byte(0x1234, 0x42)
+    dma._machine.write_memory_byte(0x1235, 0x43)
+    dma._machine.write_memory_byte(0x1236, 0x44)
+    dma._machine.write_memory_byte(0x1237, 0x45)
+
+    # Configure the DMA channel 0 for reading 4 memory bytes starting 0x1234
+    dma.write_byte(0xe000, 0x34)    # Set channel 0 start address (low byte first)
+    dma.write_byte(0xe000, 0x12)
+    dma.write_byte(0xe001, 0x04)    # Set channel 0 count and read mode
+    dma.write_byte(0xe001, 0x40)
+    dma.write_byte(0xe008, 0x01)    # Enable channel 0
+
+    # Perform the transfer using channel 0
+    data = dma.dma_read(0)
+
+    # Check the result
+    assert data == [0x42, 0x43, 0x44, 0x45]
+
+
+def test_dma_write(dma):
+    # Configure the DMA channel 1 for writing 4 memory bytes starting 0x1234
+    dma.write_byte(0xe002, 0x34)    # Set channel 1 start address (low byte first)
+    dma.write_byte(0xe002, 0x12)
+    dma.write_byte(0xe003, 0x04)    # Set channel 1 count and write mode
+    dma.write_byte(0xe003, 0x80)
+    dma.write_byte(0xe008, 0x02)    # Enable channel 1
+
+    # Perform the transfer using channel 1
+    data = dma.dma_write(1, [0x42, 0x43, 0x44, 0x45])
+
+    # Check the result
+    assert dma._machine.read_memory_byte(0x1234) == 0x42
+    assert dma._machine.read_memory_byte(0x1235) == 0x43
+    assert dma._machine.read_memory_byte(0x1236) == 0x44
+    assert dma._machine.read_memory_byte(0x1237) == 0x45
+
+
+def test_dma_autoload(dma):
+    # Fill some memory bytes
+    dma._machine.write_memory_byte(0x1234, 0x42)
+    dma._machine.write_memory_byte(0x1235, 0x43)
+    dma._machine.write_memory_byte(0x1236, 0x44)
+    dma._machine.write_memory_byte(0x1237, 0x45)
+
+    # Configure the DMA channel 2 for reading 4 memory bytes starting 0x1234 with autoload
+    dma.write_byte(0xe008, 0x80)    # Raise the autoload bit
+    dma.write_byte(0xe004, 0x34)    # Set channel 2 start address (low byte first)
+    dma.write_byte(0xe004, 0x12)
+    dma.write_byte(0xe005, 0x04)    # Set channel 2 count and read mode
+    dma.write_byte(0xe005, 0x40)
+    dma.write_byte(0xe008, 0x84)    # Enable channel 2, autoload
+
+    # Perform the transfer using channel 2, and check the result
+    data = dma.dma_read(2)
+    assert data == [0x42, 0x43, 0x44, 0x45]
+
+    # Fill the same memory with another bytes
+    dma._machine.write_memory_byte(0x1234, 0x24)
+    dma._machine.write_memory_byte(0x1235, 0x25)
+    dma._machine.write_memory_byte(0x1236, 0x26)
+    dma._machine.write_memory_byte(0x1237, 0x27)
+
+    # Perform the transfer again, expecting the channel parameters are autoloaded, and check the result
+    data = dma.dma_read(2)
+    assert data == [0x24, 0x25, 0x26, 0x27]
+
+
+def test_dma_not_autoloaded(dma):
+    # Fill some memory bytes
+    dma._machine.write_memory_byte(0x1234, 0x42)
+    dma._machine.write_memory_byte(0x1235, 0x43)
+    dma._machine.write_memory_byte(0x1236, 0x44)
+    dma._machine.write_memory_byte(0x1237, 0x45)
+
+    # Configure the DMA channel 3 for reading 4 memory bytes starting 0x1234 with autoload
+    dma.write_byte(0xe008, 0x00)    # Clear the autoload bit
+    dma.write_byte(0xe006, 0x34)    # Set channel 3 start address (low byte first)
+    dma.write_byte(0xe006, 0x12)
+    dma.write_byte(0xe007, 0x04)    # Set channel 3 count and read mode
+    dma.write_byte(0xe007, 0x40)
+    dma.write_byte(0xe008, 0x08)    # Enable channel 3
+
+    # Perform the transfer using channel 3, and check the result
+    data = dma.dma_read(3)
+    assert data == [0x42, 0x43, 0x44, 0x45]
+
+    # Try running another DMA read and verify that the channel is no longer enabled
+    with pytest.raises(RuntimeError):
+        dma.dma_read(3)
+
