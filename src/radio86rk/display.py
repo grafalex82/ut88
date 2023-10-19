80936aae4b9add98fd70cec8e19addada3e6276e
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2023-10-19 19:28:56 +0300

    RK86: Add documentation for the RK86Display class

--- src/radio86rk/display.py
+++ src/radio86rk/display.py
@@ -7,8 +7,30 @@ from common.surface import DisplaySurface, CHAR_WIDTH, CHAR_HEIGHT
 
 resources_dir = os.path.join(os.path.dirname(__file__), "..", "..", "resources")
 
+# Intel 8275 commands
+CRT_COMMAND_RESET       =   0
+CRT_COMMAND_START       =   1
+CRT_COMMAND_SET_CURSOR  =   4
+
+# Intel 8275 registers
+CRT_SREG                =   1
+CRT_PREG                =   0
 
 class RK86Display:
+    """
+        This class implement a display module of Radio-86RK computer. The actual displaying and
+        font handling is delegated to DisplaySource class, while this class is focused on Intel
+        8275 chip emulated, used as CRT controller in the Radio-86RK computer. The class implements
+        registers and commands exposed by the chip.
+
+        The class supports blinking cursor (only full char blinking). Like in the chip, the cursor
+        location is handled by this class. 
+
+        Note: refer to the Intel 8275 datasheet for command and parameters description
+
+        Note: only modes sufficient for the Radio-86RK are implemented. 
+    """
+
     def __init__(self, dma):
         self._dma = dma
         self._surface = DisplaySurface(f"{resources_dir}/rk86_font.bin", 78, 30)
@@ -44,19 +66,23 @@ class RK86Display:
 
 
     def _handle_command(self, value):
-        match value >> 5:
-            case 0: # Reset
-                self._current_command = value
-                self._current_parameter = 0
-            case 1: # Start Display
-                self._burst_space_code = ((value >> 2) & 0x07) * 8 - 1
-                self._burst_count_code = ((value & 0x03) + 1) * 2
-                self._handle_start_display_command()
-            case 4: # Set Cursor Position
-                self._current_command = value
-                self._current_parameter = 0
-            case _:
-                raise IOError(f"Unsupported i8275 command: 0x{value:02x}")
+        cmd = value >> 5
+
+        if cmd == CRT_COMMAND_RESET:
+            self._current_command = value
+            self._current_parameter = 0
+
+        elif cmd == CRT_COMMAND_START:
+            self._burst_space_code = ((value >> 2) & 0x07) * 8 - 1
+            self._burst_count_code = ((value & 0x03) + 1) * 2
+            self._handle_start_display_command()
+
+        elif cmd == CRT_COMMAND_SET_CURSOR:
+            self._current_command = value
+            self._current_parameter = 0
+
+        else:
+            raise IOError(f"Unsupported i8275 command: 0x{value:02x}")
 
 
     def _handle_reset_command_parameter(self, value):
@@ -99,13 +125,16 @@ class RK86Display:
 
 
     def _handle_parameter(self, value):
-        match self._current_command:
-            case 0x00: # Reset command
-                return self._handle_reset_command_parameter(value)
-            case 0x80: # Set cursor position
-                return self._handle_set_cursor_command_parameter(value)
-            case _:
-                raise IOError("Writing i8275 parameter for an unknown command")
+        cmd = self._current_command >> 5
+
+        if cmd == CRT_COMMAND_RESET:
+            return self._handle_reset_command_parameter(value)
+            
+        elif cmd == CRT_COMMAND_SET_CURSOR:
+            return self._handle_set_cursor_command_parameter(value)
+            
+        else:
+            raise IOError("Writing i8275 parameter for an unknown command")
     
 
     def _handle_start_display_command(self):
@@ -118,21 +147,19 @@ class RK86Display:
     
 
     def read_byte(self, offset):
-        match offset:
-            case 1:
-                return self._read_status_reg()
-            case _:
-                raise MemoryError(f"Writing CRT register {offset} is not supported")        
+        if offset == CRT_SREG:
+            return self._read_status_reg()
+            
+        raise MemoryError(f"Writing CRT register {offset} is not supported")        
 
 
     def write_byte(self, offset, value):
-        match offset:
-            case 1:
-                self._handle_command(value)
-            case 0:
-                self._handle_parameter(value)
-            case _:
-                raise MemoryError(f"Writing CRT register {offset} is not supported")
+        if offset == CRT_SREG:
+            self._handle_command(value)
+        elif offset == CRT_PREG:
+            self._handle_parameter(value)
+        else:
+            raise MemoryError(f"Writing CRT register {offset} is not supported")
             
 
     def update_screen(self, screen):
